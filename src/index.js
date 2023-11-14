0818ba362f8aaefb57f12ca5f8483875baeca19e
fgarciamatar <fgarciamatar@gmail.com>
2023-11-14 11:21:01 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+//hola
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
