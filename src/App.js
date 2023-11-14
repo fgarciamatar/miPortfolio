8e58cc0697cbf2deb977fad5cabfcd5a53c19cbe
fgarciamatar <fgarciamatar@gmail.com>
2023-11-14 11:48:50 -0300

    cambios en home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import Cursor from "../src/components/Cursor";
+// import Preloader from "../src/components/Pre";
+import "./App.css";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Project from "./pages/Project";
+import Home from "./components/Homepage";
 
 function App() {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Cursor /> */}
+      <Router>
+        {/* <Preloader load={load} /> */}
+        <div className="App" id={load ? "no-scroll" : "scroll"}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
